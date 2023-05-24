15694fa67d21de5b3b9b8ab077153617b989c1ea
Dionisio <benevidescursos@gmail.com>
2023-05-24 17:36:20 -0300

    feat: adding cors

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ import "reflect-metadata";
 import clientRoutes from "./routes/client.routes";
 import { handleAppErrorMiddleware } from "./middlewares/handleAppError.middleware";
 import contactRoutes from "./routes/contact.routes";
+import cors from "cors";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 app.use("/client", clientRoutes);
 app.use("/contact", contactRoutes);
